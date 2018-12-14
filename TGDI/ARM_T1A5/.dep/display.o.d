d888e00461ca1d34ccb5e160ec606cc8b433d2a2
Luku1806 <45849506+Luku1806@users.noreply.github.com>
2018-12-14 22:42:57 +0100

    cleaned project

--- TGDI/ARM_T1A5/.dep/display.o.d
+++
@@ -1,8 +0,0 @@
-display.o: display.c include/AT91SAM7S64.h include/display.h \
-  include/stdarg.h
-
-include/AT91SAM7S64.h:
-
-include/display.h:
-
-include/stdarg.h:
--- TGDI/ARM_T1A5/.dep/main.o.d
+++
@@ -1,5 +0,0 @@
-main.o: main.c include/AT91SAM7S64.h include/display.h
-
-include/AT91SAM7S64.h:
-
-include/display.h:
