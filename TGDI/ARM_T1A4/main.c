8231f54f12d650893b3a124d513cbe19080ac2b5
Luku1806 <45849506+Luku1806@users.noreply.github.com>
2019-01-08 12:58:32 +0100

    changed pause function

--- TGDI/ARM_T1A4/main.c
+++ TGDI/ARM_T1A4/main.c
@@ -7,12 +7,24 @@
 
 #include "include/display.h"
 
+//**************************************************************
+// delay5ms(uiK) verzögert um ca. uiK * 5ms
+//**************************************************************
+void delay5ms(unsigned int uiK) {
+	volatile unsigned int uiI;
+
+	while (uiK--)				// folgende for-Schleife wird uiK-mal aufgerufen
+		for (uiI = 0; uiI < 8192; uiI++)
+			;		//  for-Schleife wird 8192-mal durchlaufen. Dies
+					//  verursacht eine Zeitverzögerung von ca. 5ms.
+}
+
 void printNumbers() {
 	unsigned char num = 0;
 	do {
 		display_set_cursor(0, 0);
 		display_printf("%d", num++);
-		delay1us(500000);
+		delay5ms(1);
 	} while (num);
 }
 
